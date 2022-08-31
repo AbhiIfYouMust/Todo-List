24659e6db9616a35ec6e7d6408b875099bcdbc2f
AbhiIfYouMust <abhigya.222dahal@gmail.com>
2022-08-31 12:49:11 +0545

    Added feature to let users toggle between projects

--- src/index.js
+++ src/index.js
@@ -10,11 +10,49 @@ const projectsDiv = document.querySelector("#projects");
 const project0 = projectGenerator("Default");
 Projects.push(project0);
 
+const Todo1 = todoGenerator("home","things to do","22/03/2022","TOP");
+const Todo2 = todoGenerator("class","things done","20/03/2022","MEDIUM");
+const Todo3 = todoGenerator("room", "my room is mine", "12/12/2021","EASY");
+
+project0.TodoList.push(Todo1);
+project0.TodoList.push(Todo2);
+project0.TodoList.push(Todo3);
+
 // Adding default to DOM
 let projectDOM0 = projectDOMGenerator(project0);
 projectsDiv.appendChild(projectDOM0);
 
 let selectedProject = Projects[0];
+
+const TODODisplay = document.querySelector("#TODO-display");
+
+let DisplayTODOs = selectedProject.TodoList;
+
+DisplayTODOs.forEach(element => {
+    let TodoDom = todoDOMGenerator(element);
+    TODODisplay.appendChild(TodoDom);
+});
+
+
+// Triggers on clicking on project div
+// Changes selectedProject and displays it's content
+projectsDiv.addEventListener('click', function(event){
+    if (event.target.className === 'project') {
+        let requiredTitle = event.target.textContent;
+        selectedProject = Projects.find(x => x.title === requiredTitle);
+
+        DisplayTODOs = selectedProject.TodoList;
+
+        //Empties div before populating to avoid duplicates
+        TODODisplay.innerHTML = "";
+
+        // Displays TODOs
+        DisplayTODOs.forEach(element => {
+            let TodoDom = todoDOMGenerator(element);
+            TODODisplay.appendChild(TodoDom);
+        });
+    };
+})
 
 // Triggers on hitting submit button on project div
 projectsDiv.addEventListener('submit', function(event) {
@@ -34,26 +72,25 @@ projectsDiv.addEventListener('submit', function(event) {
 	event.target.reset();
 });
 
-const TODODiv = document.querySelector("#TODOs");
+const TODOquery = document.querySelector("#TODO-query");
 
 // Triggers on hitting submit button on todo div
-TODODiv.addEventListener('submit', function(event) {
+TODOquery.addEventListener('submit', function(event) {
 	// Prevent default form submit
 	event.preventDefault();
 
     // Collecting user entries from FORM inputs          
-    let title = TODODiv.querySelector('#title').value;
-    let description = TODODiv.querySelector('#description').value;
-    let dueDate = TODODiv.querySelector('#dueDate').value;
-    let priority = TODODiv.querySelector('#priority').value;
+    let title = TODOquery.querySelector('#title').value;
+    let description = TODOquery.querySelector('#description').value;
+    let dueDate = TODOquery.querySelector('#dueDate').value;
+    let priority = TODOquery.querySelector('#priority').value;
 
     let TODO = todoGenerator(title, description, dueDate, priority);
 
-    console.log(TODO);
     selectedProject.TodoList.push(TODO);
 
     let TODOdom = todoDOMGenerator(TODO);
-    TODODiv.appendChild(TODOdom);
+    TODODisplay.appendChild(TODOdom);
 
 	// Clear the form fields
 	event.target.reset();
